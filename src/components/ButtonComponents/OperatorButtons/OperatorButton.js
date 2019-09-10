4e165257b1cc197be9419e86169324c4bf5f98d7
Nathan Melby <nathan@Nathans-Mac-mini.local>
2019-09-10 17:47:14 -0500

    pair programmed with Meera. 0 button is tricky
    Co-authored-by: Meera Andersen <38503526+meera-andersen@users.noreply.github.com>

--- src/components/ButtonComponents/OperatorButtons/OperatorButton.js
+++ src/components/ButtonComponents/OperatorButtons/OperatorButton.js
@@ -1,9 +1,18 @@
 import React from "react";
 
-const OperatorButton = () => {
+const OperatorButton = (props) => {
+  
+    console.log(props);
   return (
     <>
-      {/* Display a button element rendering the data being passed down from the parent container on props */}
-    </>
-  );
+    <button className="operator_button">
+      <span role = "operator symbol" aria-label = "operator symbol">
+      {props.operators.char}
+      </span>
+    </button>
+   </>
+  )
+  
 };
+
+export default OperatorButton;
